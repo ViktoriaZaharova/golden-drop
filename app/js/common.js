7e58588a61c6745650c5e2718000efa0a71ecbc4
Viktoria <zaharova181114@gmail.com>
2022-07-31 22:22:37 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.home-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    dots: true,
+    appendDots: '.home-slider__nav',
+    appendArrows: '.home-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
