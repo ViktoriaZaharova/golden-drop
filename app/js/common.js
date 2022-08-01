45eb0c8069abbd12a9b95152474d98f5dd746e8b
Viktoria <zaharova181114@gmail.com>
2022-08-01 21:30:28 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -9,3 +9,10 @@ $('.home-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
+
+$('.events-slider').slick({
+    slidesToShow: 3,
+    appendArrows: '.events-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
